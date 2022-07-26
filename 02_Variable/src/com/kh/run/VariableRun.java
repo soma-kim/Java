790188900bf2fa36a2f3010570885102211341c7
user1 <user1@DESKTOP-3DIAQTA>
2022-07-26 17:21:47 +0900

    7/26

---
+++ 02_Variable/src/com/kh/run/VariableRun.java
@@ -0,0 +1,21 @@
+package com.kh.run;
+
+import com.kh.variable.A_Variable;
+
+public class VariableRun {
+	
+	public static void main(String[] args) {
+		
+		// A_Variable 클래스의 printSalary 메소드를 호출
+		
+		// 1. A_Variable 클래스를 대변할이름을 하나 만들기
+		// => 클래스명 대변할이름 = new 클래스명();
+		A_Variable a = new A_Variable();
+		
+		// 3. printSalary 메소트 호출
+		a.printSalary();
+		
+		
+	}
+
+}
---
+++ 02_Variable/src/com/kh/variable/A_Variable.java
@@ -0,0 +1,26 @@
+package com.kh.variable;
+
+public class A_Variable {
+	
+	// 변수의 필요성
+	// 시급과 근무 시간과 근무 일수를 곱해서 월급을 계산하는 프로그램
+	public void printSalary() {
+		
+		System.out.println("===== 변수 사용 전 =====");
+		
+		System.out.println("시급: 9160원");
+		System.out.println("근무시간: 6시간");
+		System.out.println("근무일수: 5일");
+		
+		// 월급 = 시급 x 근무시간 x 근무일수
+		// 홍길동: 00000 원 형식으로 출력
+		
+		System.out.println("강성훈: 9160 x 6 x 5");
+		System.out.println("구샛별: 9160 x 6 x 5");
+		System.out.println("김도연: 9160 x 6 x 5");
+		System.out.println("김수빈: 9160 x 6 x 5");
+		
+	}
+	
+
+}
