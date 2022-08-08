fda621c17b01780d720b39260488cd26756229ea
user1 <user1@DESKTOP-3DIAQTA>
2022-08-08 12:22:50 +0900

    8/8 morning

--- 08_Inheritance/src/com/kh/chap01/beforeVSafter/after/run/AfterRun.java
+++ 08_Inheritance/src/com/kh/chap01/beforeVSafter/after/run/AfterRun.java
@@ -35,6 +35,10 @@ public class AfterRun {
 		System.out.println(d.information());
 		System.out.println(s.information());
 		System.out.println(t.information());
+		// 실행하고자 하는 메소드가 부모 클래스에만 있고 자식 클래스에는 없음
+		// 자식 클래스에서는 부모 클래스의 내용을 가져다 쓸 수 있지만
+		// 부모 클래스에서는 자식 클래스의 내용을 가져다 쓸 수가 없다!! => 상속 구조의 가장 핵심적인 특징
+		// 자식 클래스 입장에서 물려받은 부모 클래스의 메소드를 내 입맛대로 재정의해서 쓸 수 있음 (오버라이딩)
 
 	}
 
