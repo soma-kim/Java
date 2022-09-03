032f0165d1407cf218e45b32cdc3fcee7834d4fd
subin <shallwetyping@gmail.com>
2022-09-03 23:18:48 +0900

    9/3 done homework

--- 01_Java-workspace/15_MVC_MemberManager/src/com/kh/controller/MemberController.java
+++ 01_Java-workspace/15_MVC_MemberManager/src/com/kh/controller/MemberController.java
@@ -5,35 +5,131 @@ import com.kh.model.vo.Member;
 
 public class MemberController {
 	
-	ArrayList<Member> list = new ArrayList<>();
+	private ArrayList<Member> list = new ArrayList<>();
 	
+	{
+		// 초기화 블록
+		list.add(new Member(1, "user01", "pass01", "김유저", 28, 'F', "user01@naver.com", "010-1234-5678"));
+		list.add(new Member(2, "user02", "pass02", "이사람", 20, 'M', "user02@gmail.co.kr", "010-2222-9999"));
+		list.add(new Member(3, "user03", "pass03", "최고당", 25, 'F', "user03@daum.net", "010-4812-4212"));
+		list.add(new Member(4, "user04", "pass04", "최저당", 21, 'M', "user04@facebook.co.kr", "010-8412-4105"));
+		
+	}
+	
+	// 아이디 중복 체크 처리, 사용 중인 아이디가 있을 시 1 반환
+	public int idCheck(String userId) {
+		int result = 0;
+		
+		for(int i = 0; i < list.size(); i++) {
+			
+			if(list.get(i).getUserId().equals(userId)) {
+				result++;
+			}
+					
+		}
+
+		return result;
+		
+	}
+	
+	// 회원 추가
 	public int insertMember(String userId, String userPwd, String userName, int age, char gender, String email, String phone) {
 		
 		int before = list.size();
 		
-		list.add(new Member(userId, userPwd, userName, age, gender, email, phone));
+		// 초기화 블록 적용해서 시작 값을 3으로 해 줘야 됨
+		int userNo = list.size();
+		userNo = list.size() + 1;
+		
+		// 문제점1 => 삭제 메소드에서 해결!
+		// 현재 중간 번호의 유저를 삭제했을 때 기존에 설정해 준 userNo가 그대로 유지됨
+		// => 1~4까지 존재할 때 3번째를 지우면 남은 유저의 번호는 1, 2, 4
+		//    이 상태에서 유저를 한 번 더 추가할 경우 userNo 4가 2명이 됨
+		
+		list.add(new Member(userNo, userId, userPwd, userName, age, gender, email, phone));
 		
 		return list.size() - before;
 	}
 	
+	// 회원 전체 조회
 	public ArrayList<Member> selectMemberList() {
 		return list;
 	}
 	
-	public void searchMemberById() {
+	// 회원 아이디 검색
+	public ArrayList<Member> searchMemberById(String searchedId) {
+		
+		ArrayList<Member> searchedIdList = new ArrayList<>();
+		
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getUserId().equals(searchedId)) {
+				searchedIdList.add(list.get(i));
+			}
+		}
+		
+		return searchedIdList;
 		
 	}
 	
-	public void searchMemberByName() {
+	// 회원 이름 키워드 검색
+	public ArrayList<Member> searchMemberByName(String searchedName) {
 		
+		ArrayList<Member> searchedNameList = new ArrayList<>();
+		
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getUserName().contains(searchedName)) {
+				searchedNameList.add(list.get(i));
+			}
+		}
+		
+		return searchedNameList;
 	}
 	
-	public void initMemberPwd() {
+	// 회원 비밀번호 초기화
+	public int initMemberPwd(String userId) {
 		
+		int result = 0;
+		
+		for(int i = 0; i < list.size(); i++) {
+			
+			if(list.get(i).getUserId().equals(userId)) {
+				list.set(i, new Member(list.get(i).getUserNo(), userId, "P@s$W0rd!", list.get(i).getUserName(),
+									   list.get(i).getAge(), list.get(i).getGender(), list.get(i).getEmail(), list.get(i).getPhone()));
+			result++;
+			
+			}
+		}
+		
+		return result;
 	}
 	
-	public void deleteMember() {
+	// 회원 삭제
+	public int deleteMember(String userId) {
 		
+		int result = 0;
+		
+		for(int i = 0; i < list.size(); i++) {
+			
+			if(list.get(i).getUserId().equals(userId)) {
+			
+				list.remove(i--);
+				result++;
+				
+			}
+			
+			// 삭제했을 때 지워진 데이터보다 큰 인덱스의 UserNo를 result만큼 빼 주면 되지 않을까?
+			int userNo = list.get(i).getUserNo();
+			
+			// 삭제되었을 때 userNo를 result의 수만큼 당겨 줘야 됨
+			if(result != 0 && userNo > i+1) {
+						
+				list.set(i, new Member(userNo - result, list.get(i).getUserId(), list.get(i).getUserPwd(), list.get(i).getUserName(),
+									  list.get(i).getAge(), list.get(i).getGender(), list.get(i).getEmail(), list.get(i).getPhone()));
+			} // => 진행했을 때 잘됨!!
+			
+		}
+		
+		return result;
 	}
 
 }
--- 01_Java-workspace/15_MVC_MemberManager/src/com/kh/view/MemberView.java
+++ 01_Java-workspace/15_MVC_MemberManager/src/com/kh/view/MemberView.java
@@ -8,10 +8,12 @@ import com.kh.model.vo.Member;
 
 public class MemberView {
 	
-	Scanner sc = new Scanner(System.in);
-	MemberController mc = new MemberController();
+	private Scanner sc = new Scanner(System.in);
+	private MemberController mc = new MemberController();
 	
 	public void mainMenu() {
+		
+		for(int i = 0; i < 5; i++) {
 		
 		System.out.print("아이디: ");
 		String id = sc.nextLine();
@@ -19,59 +21,74 @@ public class MemberView {
 		System.out.print("비밀번호: ");
 		String pwd = sc.nextLine();
 		
-		if(id.equals("admin") && pwd.equals("P@S$W0rd!")) {
-			
-			while(true) {
-			System.out.println("========= 메뉴 ========");
-			System.out.println("1. 회원 추가");
-			System.out.println("2. 회원 전체조회");
-			System.out.println("3. 회원 아이디 검색");
-			System.out.println("4. 회원 이름 키워드 검색");
-			System.out.println("5. 회원 비밀번호 초기화");
-			System.out.println("6. 회원 삭제");
-			System.out.println("0. 프로그램 로그아웃");
-			
-			System.out.println("=====================");
-			System.out.print("메뉴 입력: ");
-			int menu = sc.nextInt();
-			sc.nextLine();
-			
-			switch(menu) {
-			case 1 : insertMember();
-					 break;
-			
-			case 2 : selectMemberList();
-			 		 break;
-			 
-			case 3 : searchMemberById();
-			 		 break;
-			 
-			case 4 : searchMemberByName();
-					 break;
-			 
-			case 5 : initMemberPwd();
-					 break;
-			 
-			case 6 : deleteMember();
-			 		 break;
-			 
-			case 0 : System.out.println("프로그램을 종료합니다.");
-					 return; 
+			if(id.equals("admin") && pwd.equals("P@s$W0rd!")) {
+				
+				while(true) {
+				System.out.println("========= 메뉴 ========");
+				System.out.println("1. 회원 추가");
+				System.out.println("2. 회원 전체조회");
+				System.out.println("3. 회원 아이디 검색");
+				System.out.println("4. 회원 이름 키워드 검색");
+				System.out.println("5. 회원 비밀번호 초기화");
+				System.out.println("6. 회원 삭제");
+				System.out.println("0. 프로그램 로그아웃");
+				
+				System.out.println("=====================");
+				System.out.print("메뉴 입력: ");
+				int menu = sc.nextInt();
+				sc.nextLine();
+				
+				switch(menu) {
+				case 1 : insertMember();
+						 break;
+				
+				case 2 : selectMemberList();
+				 		 break;
+				 
+				case 3 : searchMemberById();
+				 		 break;
+				 
+				case 4 : searchMemberByName();
+						 break;
+				 
+				case 5 : initMemberPwd();
+						 break;
+				 
+				case 6 : deleteMember();
+				 		 break;
+				 
+				case 0 : System.out.println("프로그램을 종료합니다.");
+						 return; 
+				}
+				
+				}
+				
+			} else {
+				if(i != 4) {
+				System.out.println(i+2 + "번째 시도입니다. 로그인 정보가 맞지 않으므로 다시 입력해 주세요.");
+				continue;
+				} else {
+					System.out.println("다섯 번 이상 틀린 정보를 입력하셨으므로 프로그램을 종료합니다.");
+					return;
+				}
 			}
-			
-			}
-			
-		} else {
-			System.out.println("로그인 정보가 맞지 않습니다. 다시 입력해 주세요.");
-			mainMenu();
-			return;
 		}
+		
 	}
 	
+	// 회원 추가 화면
 	public void insertMember() {
 		System.out.println("== 새로운 멤버 추가 ==");
 		System.out.print("아이디: ");
 		String userId = sc.nextLine();
+		
+		int idCheck = mc.idCheck(userId);
+		
+		if(idCheck != 0) {
+			System.out.println("이미 존재하는 아이디입니다. 다른 아이디를 입력하세요.");
+			insertMember();
+			return;
+		}
 		
 		System.out.print("비밀번호: ");
 		String userPwd = sc.nextLine();
@@ -83,8 +100,20 @@ public class MemberView {
 		int age = sc.nextInt();
 		sc.nextLine();
 		
-		System.out.print("성별(M/F): ");
-		char gender = sc.nextLine().toUpperCase().charAt(0);
+		// 성별 확인받고, M/F가 아닐 경우 다시 입력받기 위해 변수만 따로 빼고 판별식은 while로 묶음
+				char gender = 0;
+				
+				while (true) {
+				
+				System.out.print("성별(M/F): ");
+				gender = sc.nextLine().toUpperCase().charAt(0);
+				
+				if(gender != 'M' && gender != 'F') {
+					System.out.println("성별 재확인 후 알파벳 M(m) 또는 F(f)으로 입력하세요.");
+					continue;
+				}
+					break;
+				}
 		
 		System.out.print("이메일: ");
 		String email = sc.nextLine();
@@ -102,14 +131,22 @@ public class MemberView {
 
 	}
 	
+	// 회원 전체 조회 화면
 	public void selectMemberList() {
-		System.out.println("=== 멤버 목록 ===");
 		
 		ArrayList<Member> list = mc.selectMemberList();
 		
+		System.out.println("전체 회원을 조회합니다.");
+		System.out.println("현재 등록된 회원은 " + list.size() + "명입니다.");
+		System.out.println("=== 멤버 목록 ===");
+
+		
 		if(list.isEmpty()) {
+			
 			System.out.println("등록된 멤버가 없습니다.");
+			
 		} else {
+			
 			for(int i = 0; i < list.size(); i++) {
 				System.out.println(list.get(i));
 			}
@@ -117,20 +154,85 @@ public class MemberView {
 
 	}
 	
+	// 회원 아이디 검색 화면
 	public void searchMemberById() {
 		
+		System.out.println("검색어와 회원의 아이디를 검색합니다.");
+		System.out.print("검색할 아이디: ");
+		String searchedId = sc.nextLine();
+		
+		ArrayList<Member> searchedList = mc.searchMemberById(searchedId);
+		
+		if(searchedList.isEmpty()) {
+			
+			System.out.println("일치하는 아이디가 없습니다. 다시 입력해 주세요.");
+			searchMemberById();
+			return;
+			
+		} else {
+			
+			for(int i = 0; i < searchedList.size(); i++) {
+				System.out.println(searchedList.get(i));
+			}
+			
+		}
+
 	}
 	
+	// 회원 이름 키워드 검색 화면
 	public void searchMemberByName() {
 		
-	}
-	
-	public void initMemberPwd() {
+		System.out.println("검색한 키워드를 포함하는 이름이 있는 경우 해당 회원 정보를 나타냅니다.");
+		System.out.print("검색할 이름: ");
+		String searchedName = sc.nextLine();
+		
+		ArrayList<Member> searchedNameList = mc.searchMemberByName(searchedName);
+		
+		for(int i = 0; i < searchedNameList.size(); i++) {
+			System.out.println(searchedNameList.get(i));
+		}
 		
 	}
 	
+	// 회원 비밀번호 초기화 화면
+	public void initMemberPwd() {
+		
+		System.out.println("비밀번호를 'passwordKH@123' 으로 초기화합니다.");
+		System.out.print("비밀번호를 초기화할 아이디: ");
+		String userId = sc.nextLine();
+		
+		int result = mc.initMemberPwd(userId);
+		
+		if (result > 0) {
+			 System.out.println("성공적으로 초기화되었습니다.");
+		} else {
+			System.out.println("일치하는 아이디가 없으므로 초기화할 수 없습니다. 다시 시도하세요.");
+			initMemberPwd();
+			return;
+		}
+		
+	}
+	
+	// 회원 삭제 화면
 	public void deleteMember() {
+		System.out.println("회원을 삭제합니다.");
+		System.out.print("삭제할 회원의 아이디: ");
+		String userId = sc.nextLine();
+		
+		System.out.print("삭제 후 복구할 수 없습니다. 정말 삭제하시겠습니까? (Y/N): ");
+		char answer = sc.nextLine().toUpperCase().charAt(0);
+		
+		int result = mc.deleteMember(userId);
+		
+		if(answer == 'Y') {
+			if(result > 0) {
+				System.out.println("성공적으로 삭제되었습니다.");
+			} else {
+				System.out.println("일치하는 아이디가 없으므로 삭제할 수 없습니다.");
+			}
+		
+		}
 		
 	}
 
-} // MemberView
+}
